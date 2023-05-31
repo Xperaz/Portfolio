327043b0af8697d7d40959138db2cfcddf2571ec
Azedine ouhadou <aouhadou@student.1337.ma>
2023-05-31 11:54:38 +0100

    setup and header completed

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import Head from 'next/head';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Aouhadou&apos;s Portfolio</title>
+      </Head>
+
+      <Header />
+
+      {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+      
+      {/* Contact Me */}
+
+    </div>
+  )
+}
